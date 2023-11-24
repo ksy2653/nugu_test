b24c7a425eee8862a49ff305c8bd6f7a03c5968a
ksy2653 <kim2653seok@gmail.com>
2023-11-24 11:57:15 +0000

    fix: index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello, GCP!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-  
+// 서버를 0.0.0.0 주소로 바인딩하여 외부에서 접근 가능하게 함
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
